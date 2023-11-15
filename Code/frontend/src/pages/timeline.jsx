bb04795f279aadea336fb02964342136eaae8ba8
SamarthPanchal14 <samarth.panchal14112002@gmail.com>
2023-11-15 18:34:30 +0530

    add timeline

---
+++ Code/frontend/src/pages/timeline.jsx
@@ -0,0 +1,76 @@
+export default function timeline() {
+    return (
+        <div class="main">
+    <div class="timeline">
+        <div class="container left-container">
+            <img src="images/google.png">
+            <div class="text-box">
+                <h2>Activity-1</h2>
+                <small>2018 - 2019</small>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet excepturi ducimus architecto sequi doloribus amet aliquid obcaecati, voluptates, voluptatibus nesciunt odit expedita officia nihil beatae, rerum aperiam harum. Debitis, veniam rerum vitae inventore alias nisi!
+                </p>
+                <span class="left-container-arrow"></span>
+            </div>
+        </div>
+        <div class="container right-container">
+            <img src="images/amazon.png">
+            <div class="text-box">
+                <h2>Activity-2</h2>
+                <small>2019 - 2020</small>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet excepturi ducimus architecto sequi doloribus amet aliquid obcaecati, voluptates, voluptatibus nesciunt odit expedita officia nihil beatae, rerum aperiam harum. Debitis, veniam rerum vitae inventore alias nisi!
+                </p>
+                <span class="right-container-arrow"></span>
+            </div>
+        </div>
+        <div class="container left-container">
+            <img src="images/tesla.png">
+            <div class="text-box">
+                <h2>Activity-3</h2>
+                <small>2021 - 2022</small>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet excepturi ducimus architecto sequi doloribus amet aliquid obcaecati, voluptates, voluptatibus nesciunt odit expedita officia nihil beatae, rerum aperiam harum. Debitis, veniam rerum vitae inventore alias nisi!
+                </p>
+                <span class="left-container-arrow"></span>
+            </div>
+        </div>
+        <div class="container right-container">
+            <img src="images/toyota.png">
+            <div class="text-box">
+                <h2>Activity-4</h2>
+                <small>2023 - 2024</small>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet excepturi ducimus architecto sequi doloribus amet aliquid obcaecati, voluptates, voluptatibus nesciunt odit expedita officia nihil beatae, rerum aperiam harum. Debitis, veniam rerum vitae inventore alias nisi!
+                </p>
+                <span class="right-container-arrow"></span>
+            </div>
+        </div>
+        <div class="container left-container">
+            <img src="images/flipkart.png">
+            <div class="text-box">
+                <h2>Activity-5</h2>
+                <small>2018 - 2019</small>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet excepturi ducimus architecto sequi doloribus amet aliquid obcaecati, voluptates, voluptatibus nesciunt odit expedita officia nihil beatae, rerum aperiam harum. Debitis, veniam rerum vitae inventore alias nisi!
+                </p>
+                <span class="left-container-arrow"></span>
+            </div>
+        </div>
+        <div class="container right-container">
+            <img src="images/youtube.png">
+            <div class="text-box">
+                <h2>Activity-6</h2>
+                <small>2018 - 2019</small>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet excepturi ducimus architecto sequi doloribus amet aliquid obcaecati, voluptates, voluptatibus nesciunt odit expedita officia nihil beatae, rerum aperiam harum. Debitis, veniam rerum vitae inventore alias nisi!
+                </p>
+                <span class="right-container-arrow"></span>
+            </div>
+        </div>
+    </div>
+</div>
+    )
+
+}
+
