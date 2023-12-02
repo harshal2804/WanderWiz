1d8ef4e25c82c7f00628de65046c5961c5cd7972
Harshal Patel <harshal5820@gmail.com>
2023-12-02 23:24:16 +0530

    fix: responsive design of profile page and card

--- Code/frontend/src/components/Itinerary.jsx
+++ Code/frontend/src/components/Itinerary.jsx
@@ -1,21 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "../css/Itinerary.css";
 
-export class Itinerary extends Component {
-    
-  render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
+export default function Itinerary({ title, description, imageUrl }) {
+
     return (
-      <div >
-         <div className="card m-2" style = {{width: "18rem", height: "25rem"}}>
-            <img src={imageUrl?.prefix+"500x400"+imageUrl?.suffix} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description} days trip to {title}</p>
-            </div>
-         </div>
-      </div>
-    )
-  }
-}
+      <div className="m-4 w-100 col-12 col-sm-6 col-lg-3" style={{ maxWidth: "260px" }}>
+        <div
+          className="single_advisor_profile wow fadeInUp"
+          data-wow-delay="0.2s"
+          style={{
+            minHeight: "353px",
+            visibility: "visible",
+            animationDelay: "0.2s",
+            animationName: "fadeInUp",
+          }}
+        >
+          <div className="advisor_thumb">
+            <img
+              src={imageUrl.prefix + "200x200" + imageUrl.suffix}
+              alt=""
+            />
+          </div>
 
-export default Itinerary
+          <div className="single_advisor_details_info">
+            <h6>{title}</h6>
+            <p className="designation">{description} days trip</p>
+          </div>
+        </div>
+      </div>
+    );
+}
